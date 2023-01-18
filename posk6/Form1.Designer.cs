41c34464f89bbe26ed8616a17e8a4d605814c734
DeusArtifex <deus3artifex@gmail.com>
2023-01-18 14:25:20 +0100

    Add playability

--- posk6/Form1.Designer.cs
+++ posk6/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.board = new System.Windows.Forms.TableLayoutPanel();
             this.choice = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // board
@@ -42,7 +43,7 @@
             this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 35F));
             this.board.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.board.Location = new System.Drawing.Point(280, 71);
             this.board.Name = "board";
@@ -66,7 +67,7 @@
             this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 33F));
             this.choice.Location = new System.Drawing.Point(280, 35);
             this.choice.Name = "choice";
             this.choice.RowCount = 1;
@@ -74,11 +75,21 @@
             this.choice.Size = new System.Drawing.Size(218, 32);
             this.choice.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(629, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.choice);
             this.Controls.Add(this.board);
             this.Name = "Form1";
@@ -91,6 +102,7 @@
 
         private System.Windows.Forms.TableLayoutPanel board;
         private System.Windows.Forms.TableLayoutPanel choice;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- posk6/Form1.cs
+++ posk6/Form1.cs
@@ -14,17 +14,36 @@ namespace posk6
     {
         private Board gameBoard;
         private Choice choiceBoard;
+        private Color playerColor;
+        private Color playerOne = Color.CornflowerBlue;
+        private Color playerTwo = Color.Coral;
 
         public Form1()
         {
             InitializeComponent();
             gameBoard = new Board(board);
             choiceBoard = new Choice(choice, new EventHandler(choice_Click));
+            playerColor = playerOne;
         }
 
         private void choice_Click(object sender, EventArgs e)
         {
-            gameBoard.InsertToken(Convert.ToInt32(((Button)sender).Name));
+            int filledCol = Convert.ToInt32(((Button)sender).Name);
+            int filledRow = gameBoard.InsertToken(filledCol, playerColor);
+            bool colFilled = gameBoard.GetFilledRows(filledCol) == 6;
+            if (colFilled) { ((Button)sender).Enabled = false; }
+            if (gameBoard.WinConditions(filledCol, filledRow, playerColor))
+            {
+                choice.Enabled = false;
+            }
+            if(playerColor == playerOne)
+            {
+                playerColor = playerTwo;
+            }
+            else
+            {
+                playerColor = playerOne;
+            }
         }
     }
     public class Board
@@ -46,10 +65,11 @@ namespace posk6
                 }
             }
         }
-        public void InsertToken(int column)
+        public int InsertToken(int column, Color color)
         {
             int row = columns[column].Count - (1 + GetFilledRows(column));
-            columns[column][row].BackColor = Color.Blue;
+            columns[column][row].BackColor = color;
+            return row;
         }
         public int GetFilledRows(int column)
         {
@@ -60,6 +80,93 @@ namespace posk6
             }
             return count;
         }
+        public bool WinConditions(int col, int row, Color color)
+        {
+            bool rows = CheckRow(row, color);
+            bool columns = CheckCol(col, color);
+            bool diags = CheckDiags(col, row, color);
+            return rows || columns || diags;
+        }
+        private bool CheckRow(int row, Color color)
+        {
+            int count = 0;
+            foreach(var column in columns)
+            {
+                if (column[row].BackColor == color)
+                {
+                    count++;
+                }
+                if(count == 4) { return true; }
+            }
+            return false;
+        }
+        private bool CheckCol(int col, Color color)
+        {
+            int count = 0;
+            foreach (var row in columns[col])
+            {
+                if (row.BackColor == color) { count++; }
+                if (count == 4) { return true; }
+            }
+            return false;
+        }
+        private bool CheckDiags(int col, int row, Color color)
+        {
+            List<Label> diag1 = GetDiag(col, row, 1);
+            List<Label> diag2 = GetDiag(col, row, -1);
+            if(CheckDiag(diag1, color) || CheckDiag(diag2, color)) { return true; }
+            return false;
+        }
+        private bool CheckDiag(List<Label> diag, Color color)
+        {
+            int count = 0;
+            if(diag == null) { return false; }
+            foreach (var field in diag)
+            {
+                if(field.BackColor == color) { count++; }
+                if (count == 4) { return true; }
+            }
+            return false;
+        }
+        private List<Label> GetDiag(int col, int row, int dirLR)
+        {
+            int currRow = row;
+            int currCol = col;
+            int dirUD = 1;
+            List<Label> diag = new List<Label>();
+            for(int i = 0; i < 6; i++)
+            {
+                try
+                {
+                    diag.Add(columns[currCol][currRow]);
+                }
+                catch(ArgumentOutOfRangeException)
+                {
+                    break;
+                }
+                if(i == 5) { break; }
+                int newRow = currRow += dirUD;
+                int newCol = currCol += dirLR;
+                bool maxRow = newRow == 6;
+                bool maxCol = newCol == 7;
+                bool minRow = currRow == 0 && newRow < 0;
+                bool minCol = currCol == 0 && newCol < 0;
+                if (minRow || minCol || maxCol || maxRow)
+                {
+                    dirUD *= -1;
+                    dirLR *= -1;
+                    currCol = col + dirLR;
+                    currRow = row + dirUD;
+                    continue;
+                }
+                currCol = newCol;
+                currRow = newRow;
+            }
+            if(diag.Count < 4) { return null; }
+            else { return diag; }
+
+        }
+
     }
     public class Choice
     {
