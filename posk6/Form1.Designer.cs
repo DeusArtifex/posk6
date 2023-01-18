8ec881bfa779c4b5462e4207d13bce40e7505f9e
DeusArtifex <deus3artifex@gmail.com>
2023-01-18 12:38:04 +0100

    Initial commit

---
+++ posk6/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace posk6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.board = new System.Windows.Forms.TableLayoutPanel();
+            this.choice = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.board.ColumnCount = 7;
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.board.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.board.Location = new System.Drawing.Point(280, 71);
+            this.board.Name = "board";
+            this.board.RowCount = 6;
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.board.Size = new System.Drawing.Size(218, 187);
+            this.board.TabIndex = 0;
+            // 
+            // choice
+            // 
+            this.choice.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.choice.ColumnCount = 7;
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.choice.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.choice.Location = new System.Drawing.Point(280, 35);
+            this.choice.Name = "choice";
+            this.choice.RowCount = 1;
+            this.choice.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.choice.Size = new System.Drawing.Size(218, 32);
+            this.choice.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.choice);
+            this.Controls.Add(this.board);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel board;
+        private System.Windows.Forms.TableLayoutPanel choice;
+    }
+}
+
---
+++ posk6/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace posk6
+{
+    public partial class Form1 : Form
+    {
+        private Board gameBoard;
+        private Choice choiceBoard;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameBoard = new Board(board);
+            choiceBoard = new Choice(choice, new EventHandler(choice_Click));
+        }
+
+        private void choice_Click(object sender, EventArgs e)
+        {
+            gameBoard.InsertToken(Convert.ToInt32(((Button)sender).Name));
+        }
+    }
+    public class Board
+    {
+        private List<List<Label>> columns;
+
+        public Board(TableLayoutPanel board)
+        {
+            columns = new List<List<Label>>();
+            for(int i = 0; i < 7; i++)
+            {
+                columns.Add(new List<Label>());
+                for (int j = 0; j < 6; j++)
+                {
+                    columns[i].Add(new Label());
+                    columns[i][j].BackColor = Color.Gray;
+                    columns[i][j].Dock = DockStyle.Fill;
+                    board.Controls.Add(columns[i][j], i, j);
+                }
+            }
+        }
+        public void InsertToken(int column)
+        {
+            int row = columns[column].Count - (1 + GetFilledRows(column));
+            columns[column][row].BackColor = Color.Blue;
+        }
+        public int GetFilledRows(int column)
+        {
+            int count = columns[column].Count;
+            foreach(var row in columns[column])
+            {
+                if (row.BackColor == Color.Gray) { count--; }
+            }
+            return count;
+        }
+    }
+    public class Choice
+    {
+        private List<Button> buttons;
+
+        public Choice(TableLayoutPanel choice, EventHandler choice_Handler)
+        {
+            buttons = new List<Button>();
+            for (int i = 0; i < choice.ColumnCount; i++)
+            {
+                buttons.Add(new Button());
+                choice.Controls.Add(buttons[i]);
+                buttons[i].Click += choice_Handler;
+                buttons[i].Dock = DockStyle.Fill;
+                buttons[i].Text = "▼";
+                buttons[i].Margin = Padding.Empty;
+                buttons[i].Name = i.ToString();
+            }
+        }
+    }
+}
